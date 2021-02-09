084cd394af22fed7bdd26ef1faddd9f7ed5b4593
Craig Thomas <cthomas@c2labs.com>
2021-02-08 19:19:52 -0500

    update django config

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base', # adds the base app
+    'base',
     'storages'
 ]
 
